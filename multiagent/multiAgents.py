e9510c57f5a85e7d34ceeb82a569301df3646807
Jaisinghani <piyushajaisinghani@gmail.com>
2018-10-17 22:23:04 -0400

    Implemented Alpha Beta Pruning

--- multiagent/multiAgents.py
+++ multiagent/multiAgents.py
@@ -177,9 +177,8 @@ class MinimaxAgent(MultiAgentSearchAgent):
             )
             return (max if self.isPacman(state, agent) else min)(successors)
 
-            # return the best of pacman's possible moves
-
-        return max(gameState.getLegalActions(0),key=lambda x: minimax(gameState.generateSuccessor(0, x), 0, 1))
+         # return the best of pacman's possible moves
+        return max(gameState.getLegalActions(0), key=lambda x: minimax(gameState.generateSuccessor(0, x), 0, 1))
         #util.raiseNotDefined()
 
 class AlphaBetaAgent(MultiAgentSearchAgent):
@@ -192,7 +191,42 @@ class AlphaBetaAgent(MultiAgentSearchAgent):
           Returns the minimax action using self.depth and self.evaluationFunction
         """
         "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        #util.raiseNotDefined()
+        def dispatch(state, depth, agent, A=float("-inf"), B=float("inf")):
+            if agent == state.getNumAgents():  # next depth
+                depth += 1
+                agent = 0
+
+            if self.isTerminal(state, depth, agent):  # dead end
+                return self.evaluationFunction(state), None
+
+            if self.isPacman(state, agent):
+                return getValue(state, depth, agent, A, B, float('-inf'), max)
+            else:
+                return getValue(state, depth, agent, A, B, float('inf'), min)
+
+        def getValue(state, depth, agent, A, B, ms, mf):
+            bestScore = ms
+            bestAction = None
+
+            for action in state.getLegalActions(agent):
+                successor = state.generateSuccessor(agent, action)
+                score, _ = dispatch(successor, depth, agent + 1, A, B)
+                bestScore, bestAction = mf((bestScore, bestAction), (score, action))
+
+                if self.isPacman(state, agent):
+                    if bestScore > B:
+                        return bestScore, bestAction
+                    A = mf(A, bestScore)
+                else:
+                    if bestScore < A:
+                        return bestScore, bestAction
+                    B = mf(B, bestScore)
+
+            return bestScore, bestAction
+
+        _, action = dispatch(gameState, 0, 0)
+        return action
 
 class ExpectimaxAgent(MultiAgentSearchAgent):
     """
